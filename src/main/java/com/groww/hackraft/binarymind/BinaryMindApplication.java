3060a79acf8713f47f5f892aac120b9eeeb0e9a5
Satya Vinjamuri <svinjamuri@groww.in>
2023-09-13 14:15:41 +0530

    first commit

---
+++ src/main/java/com/groww/hackraft/binarymind/BinaryMindApplication.java
@@ -0,0 +1,13 @@
+package com.groww.hackraft.binarymind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinaryMindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinaryMindApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groww/hackraft/binarymind/BinaryMindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groww.hackraft.binarymind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinaryMindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
